289399081050998804451ce63dfe188112407ea2
Laerte Neto <laerte182@hotmail.com>
2021-06-05 16:54:29 +0100

    Domain model, database seed

--- backend/src/main/java/com/laerte/dtvendas/config/SecurityConfig.java
+++ backend/src/main/java/com/laerte/dtvendas/config/SecurityConfig.java
@@ -3,6 +3,9 @@ package com.laerte.dtvendas.config;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
