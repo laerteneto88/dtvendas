d2cd3894d7da997c18b66d0a1cfb67e33d6031f6
Laerte Neto <laerte182@hotmail.com>
2021-06-05 18:17:13 +0100

    Group by search

--- backend/src/main/java/com/laerte/dtvendas/services/SaleService.java
+++ backend/src/main/java/com/laerte/dtvendas/services/SaleService.java
@@ -1,5 +1,7 @@
 package com.laerte.dtvendas.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.laerte.dtvendas.dto.SaleDTO;
+import com.laerte.dtvendas.dto.SaleSuccessDTO;
+import com.laerte.dtvendas.dto.SaleSumDTO;
 import com.laerte.dtvendas.entities.Sale;
 import com.laerte.dtvendas.repositories.SaleRepository;
 import com.laerte.dtvendas.repositories.SellerRepository;
@@ -26,4 +30,14 @@ public class SaleService {
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
 	}	
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller(){
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller(){
+		return repository.successGroupedBySeller();
+	}
 }
