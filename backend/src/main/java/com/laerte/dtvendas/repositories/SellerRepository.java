55c04ef2c720048d04c2fc19d88a296dd3612b79
Laerte Neto <laerte182@hotmail.com>
2021-06-05 17:31:05 +0100

    Layers

---
+++ backend/src/main/java/com/laerte/dtvendas/repositories/SellerRepository.java
@@ -0,0 +1,8 @@
+package com.laerte.dtvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.laerte.dtvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
---
+++ backend/src/main/java/com/laerte/dtvendas/services/SellerService.java
@@ -0,0 +1,23 @@
+package com.laerte.dtvendas.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.laerte.dtvendas.dto.SellerDTO;
+import com.laerte.dtvendas.entities.Seller;
+import com.laerte.dtvendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+	
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll() {
+		List<Seller> result = repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+	}	
+}
