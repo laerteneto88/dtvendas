c9626da3f4ad18197ec0553d931d9120077f1f51
Laerte Neto <laerte182@hotmail.com>
2021-06-05 12:31:42 +0100

    Project created

---
+++ backend/src/main/java/com/laerte/dtvendas/DtvendasApplication.java
@@ -0,0 +1,13 @@
+package com.laerte.dtvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/laerte/dtvendas/DtvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laerte.dtvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
