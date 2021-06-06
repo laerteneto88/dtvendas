06d23949357baf71f897f70cdc2a8cd192159dfc
Laerte Neto <laerte182@hotmail.com>
2021-06-06 11:18:04 +0100

    home navigation

--- frontend/src/components/NavBar/NavBar.component.tsx
+++ frontend/src/components/NavBar/NavBar.component.tsx
@@ -1,15 +1,18 @@
-import Logo from 'assets/img/logo.png';
+import Logo from 'assets/img/logo.png'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   return (
     <div className='d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm'>
       <div className='container'>
         <nav className='my-2 my-md-0 mr-md-3'>
-          <img src={Logo} alt="DevTricks" width="160" />
+          <Link to='/'>
+            <img src={Logo} alt='DevTricks' width='160' />
+          </Link>
         </nav>
       </div>
     </div>
   )
 }
 
-export default NavBar;
+export default NavBar
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,11 +1,30 @@
+import Footer from 'components/Footer/Footer.component'
+import NavBar from 'components/NavBar/NavBar.component'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>
-      home
-    </div>
+    <>
+      <NavBar />
+      <div className='container home'>
+        <div className='jumbotron'>
+          <h1 className='display-4'>DTVendas</h1>
+          <br />
+          <p className='lead'>
+            Analise o desempenho das suas vendas por diferentes perspectivas
+          </p>          
+          <p>
+            Esta aplicação consiste em exibir um dashboard a partir de dados
+            fornecidos por um back end construído com Spring Boot.
+          </p>
+          <br /><br />
+          <Link to="/dashboard" className="btn btn-primary btn-lg">Aceder ao Dashboard</Link>
+        </div>
+      </div>
+      <Footer />
+    </>
   )
 }
 
-export default Home;
+export default Home
